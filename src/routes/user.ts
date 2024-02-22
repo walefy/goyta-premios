e9dc21d45a12786d3eedecadd0fa2622379a9b1f
walefy <walefyd1@gmail.com>
2024-02-22 13:50:14 -0300

    style: remove blank lines for the route user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -14,21 +14,10 @@ const userModel = new UserModelMongo();
 const userService = new UserService(userModel, tokenAuth, passwordInstance);
 const userController = new UserController(userService);
 
-userRouter.get('/', jwtAuth(true), (req: Request, res: Response) =>
-  userController.findAll(req, res));
-
-userRouter.get('/:id', jwtAuth(), (req: Request, res: Response) =>
-  userController.findById(req, res));
-
+userRouter.get('/', jwtAuth(true), (req: Request, res: Response) => userController.findAll(req, res));
+userRouter.get('/:id', jwtAuth(), (req: Request, res: Response) => userController.findById(req, res));
 userRouter.post('/', (req: Request, res: Response) => userController.create(req, res));
-userRouter.put(
-  '/:id',
-  jwtAuth(),
-  isSameUser,
-  (req: Request, res: Response) => userController.update(req, res));
-
-userRouter.delete('/:id', jwtAuth(), isSameUser, (req: Request, res: Response) =>
-  userController.delete(req, res));
-
+userRouter.put('/:id', jwtAuth(), isSameUser, (req: Request, res: Response) => userController.update(req, res));
+userRouter.delete('/:id', jwtAuth(), isSameUser, (req: Request, res: Response) => userController.delete(req, res));
 userRouter.post('/admin', (req: Request, res: Response) => userController.createAdmin(req, res));
 userRouter.post('/login', (req: Request, res: Response) => userController.login(req, res));
